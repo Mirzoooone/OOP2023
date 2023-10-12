fff3b09abc25e85fc8b6ae12f3344dd3f81ac553
Mirzoone <90713087+Mirzoone@users.noreply.github.com>
2023-10-12 21:09:12 +0200

    Task #4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+/*Task#1
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new
@@ -23,6 +24,36 @@ public class Main {
 
         }
             scanner.close();
+    }
+}
+*/
+//Task#4
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Enter the last number: ");
+        int lastNumber = scanner.nextInt();
+
+        if (firstNumber > lastNumber) {
+            System.out.println("First number is grater than last number.");
+        }
+
+        else{
+                System.out.println("Numbers between " + firstNumber + "and" + lastNumber + ":");
+            }
+
+
+                while (firstNumber <= lastNumber) {
+                    System.out.println(firstNumber + "");
+                    firstNumber++;
+                }
+
+
+            scanner.close();
+
     }
 }
 
